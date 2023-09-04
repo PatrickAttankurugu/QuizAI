6ca706eccadb9cf016501e045ef4791eedce2f27
Patrick Attankurugu <patricka.azuma@gmail.com>
2023-09-04 15:38:42 +0000

    day 1

--- js/physics.js
+++ js/physics.js
@@ -1,51 +1,70 @@
 // Quiz functionality for the Physics page
 const PhysicsQuiz = {
-    // Initialize DOM elements and Quiz state
-    init() {
-      // Attach verification logic to quiz buttons
+  // Initialize DOM elements and Quiz state
+  init() {
+      // Attach event listeners to "Submit" buttons
       document.querySelectorAll('.submit-answer').forEach(button => {
-        button.addEventListener('click', event => {
-          const questionId = event.target.getAttribute('data-question');
-          let answer;
-          if (questionId.startsWith('mcq-')) {
-            answer = document.querySelector(`input[name="${questionId}"]:checked`).value;
-          } else {
-            answer = document.querySelector(`#${questionId} .fill-in-answer`).value;
-          }
-          this.verifyAnswer(questionId, answer);
-        });
+          button.addEventListener('click', event => {
+              const questionId = event.target.getAttribute('data-question');
+              let answer;
+              
+              if (questionId.startsWith('mcq-')) {
+                  const selectedOption = document.querySelector(`input[name="${questionId}"]:checked`);
+                  
+                  // Validate that an option has been selected
+                  if (!selectedOption) {
+                      alert('Please select an answer.');
+                      return;
+                  }
+                  
+                  answer = selectedOption.value;
+              } else {
+                  answer = document.querySelector(`#${questionId} .fill-in-answer`).value.trim();
+                  
+                  // Validate that an answer has been filled in
+                  if (!answer) {
+                      alert('Please fill in an answer.');
+                      return;
+                  }
+              }
+
+              this.verifyAnswer(questionId, answer);
+          });
       });
-    },
-  
-    // Mock-up for answer verification
-    verifyAnswer(questionId, answer) {
-      // Replace with your actual AJAX or Fetch request to GPT-3.5 Turbo
+  },
+
+  // Mock-up for answer verification using GPT-3.5 Turbo
+  verifyAnswer(questionId, answer) {
+      // Replace with your actual AJAX or Fetch request to your backend
       fetch('your-backend-endpoint', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          questionId,
-          answer
-        })
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              questionId,
+              answer
+          })
       })
       .then(response => response.json())
       .then(data => {
-        if (data.isCorrect) {
-          // Proceed to next question
-          // ... (your UI logic here)
-        } else {
-          // Show 'Try Again' message
-          // ... (your UI logic here)
-        }
+          if (data.isCorrect) {
+              // Proceed to next question or provide positive feedback
+              // ... (your UI logic here)
+              alert('Correct answer according to GPT-3.5 Turbo!');
+          } else {
+              // Show 'Try Again' message or provide negative feedback
+              // ... (your UI logic here)
+              alert('Wrong answer according to GPT-3.5 Turbo. Try again.');
+          }
       })
       .catch(error => {
-        console.error('Error verifying answer:', error);
+          console.error('Error verifying answer:', error);
       });
-    }
-  };
-  
-  // Initialize the Physics quiz functionality
+  }
+};
+
+// Initialize the Physics quiz functionality
+document.addEventListener('DOMContentLoaded', () => {
   PhysicsQuiz.init();
-  
+});
